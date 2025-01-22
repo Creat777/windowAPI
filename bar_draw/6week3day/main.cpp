09ae39fffe0adb3c422d45d867bc3003c1235ae0
Creat777 <mannam757@knou.ac.kr>
2025-01-22 11:37:27 +0900

    25.01.22
    오전 수업

--- bar_draw/6week3day/main.cpp
+++ bar_draw/6week3day/main.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
 
+// 라이브러리 추가
+#pragma comment(lib, "msimg32.lib")
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); //콜백 함수 - 윈도우 메시지 처리 함수
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevIns, LPSTR lpCmdLine, int nCmdShow)
@@ -56,8 +59,30 @@ void DrawBitmapToBuffer(HWND hWnd, HDC hBufferDC, int x, int y, HBITMAP hBit)
 	ReleaseDC(hWnd, hDC);
 }
 
+void DrawBitmapToBufferColorKey(HWND hWnd, HDC hBufferDC, int x, int y, HBITMAP hBit)
+{
+	HDC hDC = GetDC(hWnd);
+
+	HDC hTempDC = CreateCompatibleDC(hDC);
+
+	HBITMAP hOldBit = (HBITMAP)SelectObject(hTempDC, hBit);
+
+	BITMAP bitmap;
+	GetObject(hBit, sizeof(BITMAP), &bitmap);
+
+	// bitmap에서 흰색을 제거하는 함수
+	TransparentBlt(hBufferDC, x, y, bitmap.bmWidth, bitmap.bmHeight,
+		hTempDC, 0, 0, bitmap.bmWidth, bitmap.bmHeight, RGB(255, 255, 255));
+
+	SelectObject(hTempDC, hOldBit);
+	DeleteDC(hTempDC);
+
+	ReleaseDC(hWnd, hDC);
+}
+
 HBITMAP hBackBitmap; //test.bmp
 HBITMAP hBarBitmap;  //bar.bmp
+HBITMAP hBallBitmap;  //ball.bmp
 
 HANDLE hTimer;
 
@@ -67,8 +92,65 @@ RECT crt;
 
 int g_Bar_X = 200;
 
+enum BALL_DIR_X
+{
+	eRIGHT,
+	eLEFT
+};
+
+enum BALL_DIR_Y
+{
+	eDOWN,
+	eUP
+};
+
+
+
+int g_Ball_X = 0;
+int g_Ball_Y = 100;
+
 #define TIMERID 1
 
+BALL_DIR_X eDirX = eRIGHT;
+BALL_DIR_Y eDirY = eDOWN;
+
+//공을 움직이는 함수
+void BallMove()
+{
+	if (eDirX == eRIGHT)
+	{
+		g_Ball_X += 5;
+		if (g_Ball_X > (1024 - 39)) // 39는 공의 x축 크기
+		{
+			eDirX = eLEFT;
+		}
+	}
+	else if (eDirX == eLEFT)
+	{
+		g_Ball_X -= 5;
+		if (g_Ball_X < (0))
+		{
+			eDirX = eRIGHT;
+		}
+	}
+
+	if (eDirY == eDOWN)
+	{
+		g_Ball_Y += 5;
+		if (g_Ball_Y > (768 - 39)) // 39는 공의 x축 크기
+		{
+			eDirY = eUP;
+		}
+	}
+	else if (eDirY == eUP)
+	{
+		g_Ball_Y -= 5;
+		if (g_Ball_Y < (0))
+		{
+			eDirY = eDOWN;
+		}
+	}
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -83,8 +165,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 	{
+		// 이미지 가져오기
 		hBackBitmap = (HBITMAP)LoadImage(NULL, L"test.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 		hBarBitmap = (HBITMAP)LoadImage(NULL, L"Bar.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+		hBallBitmap = (HBITMAP)LoadImage(NULL, L"Ball.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 
 		hTimer = (HANDLE)SetTimer(hWnd, TIMERID, 1, NULL);
 
@@ -94,6 +178,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	
 	case WM_TIMER:
 	{
+		BallMove();
+
 		HDC hDC = GetDC(hWnd);
 
 		HDC hBufferDC = CreateCompatibleDC(hDC);
@@ -107,7 +193,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		// 그리기
 		DrawBitmapToBuffer(hWnd, hBufferDC, 0, 0, hBackBitmap);
-		DrawBitmapToBuffer(hWnd, hBufferDC, g_Bar_X, 0, hBarBitmap);
+		DrawBitmapToBuffer(hWnd, hBufferDC, g_Bar_X, 680, hBarBitmap);
+		DrawBitmapToBufferColorKey(hWnd, hBufferDC, g_Ball_X, g_Ball_Y, hBallBitmap);
 
 		SelectObject(hBufferDC, hOldBitmap);
 		DeleteDC(hBufferDC);
@@ -117,6 +204,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		InvalidateRect(hWnd, NULL, true); //WM_PAINT 요청
 	}
 	break;
+
+	case WM_KEYDOWN:
+	{
+		switch (wParam)
+		{
+		case VK_LEFT: g_Bar_X -= 10; break;
+		case VK_RIGHT: g_Bar_X += 10; break;
+		default: break;
+		}
+	}
+
 	case WM_PAINT:
 	{
 		hdc = BeginPaint(hWnd, &ps);
@@ -144,6 +242,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 		PostQuitMessage(0); // 메시지 큐에 WM_QUIT가 들어간다.
 		return 0;
+
+	default: break;
 	}
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
